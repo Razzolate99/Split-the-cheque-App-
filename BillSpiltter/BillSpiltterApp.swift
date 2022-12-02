044029c1375a390fcc94ac4441941fb22aea482f
Aaron <46139215+Razzolate99@users.noreply.github.com>
2022-12-02 15:53:59 -0500

    Initial Commit

---
+++ BillSpiltter/BillSpiltterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillSpiltterApp.swift
+//  BillSpiltter
+//
+//  Created by Aaron Ward on 2022-12-02.
+//
+
+import SwiftUI
+
+@main
+struct BillSpiltterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

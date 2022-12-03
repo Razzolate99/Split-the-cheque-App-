10c00bcdc135426042e2e07dc439fc3a4f629aa9
Aaron <46139215+Razzolate99@users.noreply.github.com>
2022-12-03 09:59:33 -0500

    First Commit

--- BillSpiltter/ContentView.swift
+++ BillSpiltter/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @State private var chequeAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson: Double {
+        // Calculations
+        // Converting types
+        let peopleCount = Double(numberOfPeople + 2) // +2 Since
+        let tipSelected = Double(tipPercentage)
+        
+        let tipValue = chequeAmount / 100 * tipSelected
+        let grandTotal = chequeAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
     }
+    var body: some View {
+            NavigationView {
+                Form{
+                    Section{
+                        TextField("Amount", value: $chequeAmount, format:
+                                .currency(code: Locale.current.currencyCode ?? "CAD"))
+                        .keyboardType(.decimalPad)
+                        
+                        Picker("Number of people", selection: $numberOfPeople){
+                            ForEach(2..<100){
+                                Text("\($0) people")
+                            }
+                        }
+                    }
+                    
+                    Section{
+                        Picker("Tip Pecentage", selection: $tipPercentage){
+                            ForEach(tipPercentages, id: \.self){
+                                Text($0, format: .percent)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                        
+                    } header: {
+                        Text("Tip percentage?")
+                            .textCase(nil)
+                            .font(.system(size: 18))
+                    }
+
+                    
+
+                    
+                    Section{
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "CAD "))
+                    }
+                }
+                .navigationTitle("We Split")
+                .navigationBarTitleDisplayMode(.inline)
+                
+                
+                
+                
+            }
+
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }

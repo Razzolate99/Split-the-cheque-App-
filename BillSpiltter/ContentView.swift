b03bd7ad9ce236d26633c1ad907402805672fdc3
Aaron <46139215+Razzolate99@users.noreply.github.com>
2022-12-11 17:13:08 -0500

    Added a tool bar to the text field that allows the user to hide the keyboard.

--- BillSpiltter/ContentView.swift
+++ BillSpiltter/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var chequeAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10,15,20,25,0]
     
@@ -27,55 +28,65 @@ struct ContentView: View {
         return amountPerPerson
     }
     var body: some View {
-            NavigationView {
-                Form{
-                    Section{
-                        TextField("Amount", value: $chequeAmount, format:
-                                .currency(code: Locale.current.currencyCode ?? "CAD"))
-                        .keyboardType(.decimalPad)
-                        
-                        Picker("Number of people", selection: $numberOfPeople){
-                            ForEach(2..<100){
-                                Text("\($0) people")
-                            }
+        NavigationView {
+            Form{
+                Section{
+                    TextField("Amount", value: $chequeAmount, format:
+                            .currency(code: Locale.current.currencyCode ?? "CAD"))
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
                         }
-                    }
-                    
-                    Section{
-                        Picker("Tip Pecentage", selection: $tipPercentage){
-                            ForEach(tipPercentages, id: \.self){
-                                Text($0, format: .percent)
-                            }
-                        }
-                        .pickerStyle(.segmented)
-                        
-                    } header: {
-                        Text("Tip percentage?")
-                            .textCase(nil)
-                            .font(.system(size: 18))
-                    }
-
-                    
-
-                    
-                    Section{
-                        Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "CAD "))
                     }
                 }
-                .navigationTitle("We Split")
-                .navigationBarTitleDisplayMode(.inline)
+                
+                Section{
+                    Picker("Tip Pecentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                } header: {
+                    Text("Tip percentage?")
+                        .textCase(nil)
+                        .font(.system(size: 18))
+                }
                 
                 
                 
                 
+                Section{
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "CAD "))
+                }
             }
-
-}
-    
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+            .navigationTitle("We Split")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+        
+        
+        
+        
     }
-}
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 }
